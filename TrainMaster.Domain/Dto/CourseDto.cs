a90944312978dd8a342bb055132fdd924d56614c
PedroIghor <pedro_pucci01@hotmail.com>
2025-05-21 15:24:12 -0300

    bug: validação dos campos para criar um curso.

--- TrainMaster.Domain/Dto/CourseDto.cs
+++ TrainMaster.Domain/Dto/CourseDto.cs
@@ -1,10 +1,16 @@
-﻿namespace TrainMaster.Domain.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrainMaster.Domain.Dto
 {
     public class CourseDto
     {
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
         public string? Name { get; set; }
+        [Required(ErrorMessage = "O campo Descrição é obrigatório.")]
         public string? Description { get; set; }
         public DateTime StartDate { get; set; }
+        //[Required(ErrorMessage = "A data de fim é obrigatória.")]
+        //[DataType(DataType.Date, ErrorMessage = "Formato de data inválido.")]
         public DateTime EndDate { get; set; }
         public bool IsActive { get; set; }        
         public int UserId { get; set; }
