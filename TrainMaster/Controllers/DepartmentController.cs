75afe4768403b07a7f5acb03e43ce4db6327ccc9
PedroIghor <pedro_pucci01@hotmail.com>
2025-05-24 16:58:21 -0300

    feat: inicio da tela de listagem de departamentos.

---
+++ TrainMaster/Controllers/DepartmentController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrainMaster.Application.UnitOfWork;
+using TrainMaster.Domain.Dto;
+using TrainMaster.Domain.Entity;
+
+namespace TrainMaster.Controllers
+{
+    [Route("Departamentos")]
+    public class DepartmentController : Controller
+    {
+        private readonly IUnitOfWorkService _serviceUoW;
+
+        public DepartmentController(IUnitOfWorkService unitOfWorkService)
+        {
+            _serviceUoW = unitOfWorkService;
+        }
+
+        [HttpGet("Index")]
+        public async Task<IActionResult> Index(int page = 1, int pageSize = 10)
+        {
+            var departamentos = await _serviceUoW.DepartmentService.Get();
+            var totalDepartamentos = departamentos.Count();
+            var departamentosPaginados = departamentos
+                .OrderBy(d => d.Name)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = (int)Math.Ceiling((double)totalDepartamentos / pageSize);
+
+            return View("Index", departamentosPaginados);
+        }
+
+        [HttpGet("Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(DepartmentEntity department)
+        {
+            if (!ModelState.IsValid)
+                return View(department);
+
+            var result = await _serviceUoW.DepartmentService.Add(department);
+            if (!result.Success)
+            {
+                ViewBag.ErrorMessage = result.Message;
+                return View(department);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        //[HttpGet("Edit/{id}")]
+        //public async Task<IActionResult> Edit(int id)
+        //{
+        //    var result = await _serviceUoW.DepartmentService.GetById(id);
+        //    if (result?.Data == null)
+        //        return NotFound();
+
+        //    ModelState.Clear();
+        //    return View("~/Views/Department/Edit.cshtml", result.Data);
+        //}
+
+        [HttpPost("Edit/{id}")]
+        public async Task<IActionResult> Edit(int id, DepartmentEntity department)
+        {
+            if (id != department.Id)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return View(department);
+
+            var result = await _serviceUoW.DepartmentService.Update(department);
+            if (!result.Success)
+            {
+                ViewBag.ErrorMessage = result.Message;
+                return View(department);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
